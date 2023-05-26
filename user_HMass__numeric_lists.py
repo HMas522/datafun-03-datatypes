20c1feff1ddbbb9500eeb3776809f127f79e1f5f
Hayley Massey <Hmassey12@outlook.com>
2023-05-26 17:13:21 -0500

    version 2

--- user_HMass__numeric_lists.py
+++ user_HMass__numeric_lists.py
@@ -11,6 +11,8 @@ import math
 
 import statistics
 
+import webbrowser
+
 from util_datafun_logger import setup_logger
 logger, logname = setup_logger(__file__)
 
@@ -22,11 +24,41 @@ logger, logname = setup_logger(__file__)
 
 # TODO: define some custom functions
 
+"""I will be making junk data from a real study from Nintendo"""
 
+# citing the source where I'm getting my data from
 
+str_response = input("Do you want to learn more about Age Distribution of Annual Nintendo Players? (y/n) ")
 
+if str_response == "y":
+    # import webbrowser at the top of the file
+    webbrowser.open("https://www.nintendolife.com/news/2022/11/switch-is-most-popular-with-22-year-olds-nintendo-says")
 
+# making a fake list of ages
 
+age_list = [
+    10
+    22
+    37
+    21
+    8
+    22
+    40
+    7
+    5
+    50
+    24
+    30
+    22
+    13
+    26
+    18
+    22
+    32
+    45
+    33
+    23
+]
 
 
 # Read log file and print it to the terminal
